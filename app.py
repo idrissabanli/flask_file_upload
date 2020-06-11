3d44227bc8f1e995d891cb16af74f57e568229d1
idris <idris.sabanli@gmail.com>
2020-06-12 01:08:23 +0400

    methods

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
